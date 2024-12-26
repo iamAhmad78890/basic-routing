06e4c91d2db78d53d15c2636847046df2bff571a
Ahmad Mujtaba <Ahmad.mujtaba78890@gmail.com>
2024-12-26 12:59:49 +0500

    routing-done

---
+++ src/pages/employee/employe1.js
@@ -0,0 +1,10 @@
+export default function Employee1(){
+    return (
+        <div>
+            <h1>
+                hamza Employee 1
+                
+            </h1>
+        </div>
+    ) 
+}
